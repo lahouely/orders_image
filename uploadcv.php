7a84a9e9ec50628c2655b4e951601bb9e7f61d86
lahouely <lahouel.youssouf@gmail.com>
2023-02-04 21:54:26 +0100

    Replaced \\ by /

--- uploadcv.php
+++ uploadcv.php
@@ -20,7 +20,7 @@ if(isset($_POST))
 		$bdd->exec('insert into cv (id,nom,prenom,niveau) values (\''.$id.'\',\''.$_POST['nom'].'\',\''.$_POST['prenom'].'\',\''.$_POST['niveau'].'\')') or die(print_r($bdd->errorInfo()));
 		
 		
-		if(move_uploaded_file($_FILES['cv']['tmp_name'],'\\var\\www\\html\\cv\\'.$id.'.doc')) //Si la fonction renvoie TRUE, c'est que �a a fonctionn�...
+		if(move_uploaded_file($_FILES['cv']['tmp_name'],'/var/www/html/cv/'.$id.'.doc')) //Si la fonction renvoie TRUE, c'est que �a a fonctionn�...
 	     {
 	          $msg= 'Upload effectue avec succes !';
 	     }
